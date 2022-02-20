56efd71b0fc54f82b0e47814a77124f142649d1b
oknappett <oknappett@gmail.com>
2022-02-20 17:18:22 +0000

    Added a threshold method to work with the opencv library

--- src/pcot/xforms/xformPctCalibrate.py
+++ src/pcot/xforms/xformPctCalibrate.py
@@ -1,6 +1,23 @@
+import cv2 as cv
+import numpy as np
+
 from pcot.datum import Datum
 from pcot.xform import xformtype, XFormType
+from pcot.xforms.tabimage import TabImage
 
+
+def thresh(img):
+    '''threshold an image, binary threshold method used'''
+    if img is None:
+        return None
+    else:
+        copy = img.copy()
+        for i in range(copy.channels):
+            c = copy.img[:, :, i]
+            c = (c * 255).astype(np.uint8)
+            ret, t = cv.threshold(c, 180, 255, cv.THRESH_BINARY)
+            copy.img[:, :, i] = t
+        return copy
 
 
 @xformtype
@@ -10,7 +27,7 @@ class XformPctCalibrate(XFormType):
 
     def __init__(self):
         super().__init__("pctCalibrate", "calibration", "0.0.0")
-        self.addInputConnector("img", Datum.IMG)
+        self.addInputConnector("", Datum.IMG)
         self.addOutputConnector("", Datum.IMG)
 
     def createTab(self, n, w):
@@ -21,6 +38,11 @@ class XformPctCalibrate(XFormType):
 
     def perform(self, node):
         data = node.getInput(0, Datum.IMG)
-        if data is not None:
+        if data is None:
+            out = None
+        else:
             img = data.copy()
-            img = img.img[:,:,1]
+            threshed = thresh(img)
+            out = threshed
+        node.out = Datum(Datum.IMG, out)
+        node.setOutput(0, node.out)
