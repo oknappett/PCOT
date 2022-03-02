e3c0a2546f68c34edb42d2b6ea501271b3fd96e2
oknappett <oknappett@gmail.com>
2022-03-02 17:24:26 +0000

    Added canny edge detector function in pct detection xform

--- src/pcot/xforms/xformPctCalibrate.py
+++ src/pcot/xforms/xformPctCalibrate.py
@@ -7,7 +7,7 @@ from pcot.xforms.tabimage import TabImage
 
 
 def thresh(img):
-    # threshold an image, binary threshold method used
+    # threshold an image, otsu threshold method used
     if img is None:
         return None
     else:
@@ -16,13 +16,32 @@ def thresh(img):
             c = copy.img[:, :, i]
             # convert the image 0-1 range to 0-255 pixels for opencv
             c = (c * 255).astype(np.uint8)
-            # blur the image with a 5x5 kernel
-            blur = cv.GaussianBlur(c, (11,11), 0)
-            lowerThresh = 180
+            # blur the image with a 11x11 kernel or bilateral filter
+            # bilateral seems to give better circles on pct
+            blur = cv.bilateralFilter(c, 10, 50, 50)
+            lowerThresh = 150
             upperThresh = 255
             ret, t = cv.threshold(blur, lowerThresh, upperThresh, cv.THRESH_OTSU)
             # set the image channel to the threshold values
             copy.img[:, :, i] = t
+        return copy
+
+
+def edge(img):
+    # edge detector on the image -> need to do before circle detection
+    if img is None:
+        return None
+    else:
+        copy = img.copy()
+        for i in range(copy.channels):
+            c = copy.img[:, :, i]
+            # convert 0-1 numpy array image tp 0-256
+            c = (c * 255).astype(np.uint8)
+            # blur image
+            blur = cv.GaussianBlur(c, (11, 11), 0)
+            # canny edge detection on blurred image
+            edges = cv.Canny(image=blur, threshold1=100, threshold2=150)
+            copy.img[:, :, i] = edges
         return copy
 
 
@@ -54,7 +73,8 @@ class XformPctCalibrate(XFormType):
             out = None
         else:
             img = data.copy()
-            threshed = thresh(img)
-            out = threshed
+            # threshed = thresh(img)
+            edges = edge(img)
+            out = edges
         node.out = Datum(Datum.IMG, out)
         node.setOutput(0, node.out)
