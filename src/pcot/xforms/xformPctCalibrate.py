aa36d8e0c2565d80a8926531a69327a1d40beba1
oknappett <oknappett@gmail.com>
2022-04-05 14:20:20 +0100

    Blob detection added, with fully functional ui for changing parameters

--- src/pcot/xforms/xformPctCalibrate.py
+++ src/pcot/xforms/xformPctCalibrate.py
@@ -1,6 +1,6 @@
-
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
 import pcot
 from pcot.datum import Datum
@@ -8,8 +8,7 @@ from pcot.xform import xformtype, XFormType
 
 
 class XformPctCalibrate(XFormType):
-    """Locates the pct and returns the centre pixel coordinate
-    of each target circle"""
+    """Class for the detection of the PCT in an image, does the perform for the detection"""
 
     def __init__(self, name, group, version):
         super().__init__(name, group, version)
@@ -19,16 +18,8 @@ class XformPctCalibrate(XFormType):
         # output type at a later date -> the centre pixels of the targets
         self.addOutputConnector("output", Datum.IMG)
 
-    def createTab(self, n, w):
-        pass
-
-    def init(self, node):
-        # output set to null initially
-        node.out = None
-        node.Kernel = 11
-
     @staticmethod
-    def thresh(self, img):
+    def thresh(img):
         # threshold an image, otsu threshold method used
         if img is None:
             return None
@@ -43,44 +34,19 @@ class XformPctCalibrate(XFormType):
                 blur = cv.bilateralFilter(c, 10, 50, 50)
                 lowerThresh = 150
                 upperThresh = 255
-                ret, t = cv.threshold(blur, lowerThresh, upperThresh, cv.THRESH_OTSU)
+                ret, t = cv.threshold(blur, lowerThresh, upperThresh, cv.THRESH_BINARY_INV)
                 # set the image channel to the threshold values
                 copy.img[:, :, i] = t
             return copy
 
     @staticmethod
-    def edge(img, kernel):
-        # edge detector on the image -> need to do before circle detection
-        k = kernel
-        if img is None:
-            return None
-        elif type(img) is np.ndarray:
-            copy = img.copy()
-            copy = (copy * 255).astype(np.uint8)
-            blur = cv.GaussianBlur(copy, (k, k), 0)
-            edges = cv.Canny(image=blur, threshold1=100, threshold2=150)
-            return edges
-        else:
-            copy = img.copy()
-            for i in range(copy.channels):
-                c = copy.img[:, :, i]
-                # convert 0-1 numpy array image tp 0-256
-                c = (c * 255).astype(np.uint8)
-                # blur image
-                blur = cv.GaussianBlur(c, (k, k), 0)
-                # canny edge detection on blurred image
-                edges = cv.Canny(image=blur, threshold1=100, threshold2=150)
-                copy.img[:, :, i] = edges
-            return copy
-
-    def Cluster(self, Circles):
+    def cluster(Circles):
         if Circles is None:
             return None
         else:
-            # loop through all circles
-            AverageCircles = []
             copy = Circles.copy()
             clustered = []
+            # loop through all circles
             for c in copy:
                 x, y, r = c[0], c[1], c[2]
                 SameCircles = []
@@ -88,8 +54,10 @@ class XformPctCalibrate(XFormType):
                     # loop through all circles and compare to current circle
                     if j is not c:
                         Jx, Jy, Jr = j[0], j[1], j[2]
-                        # if current circle centre is inside other circle then remove circle
-                        if ((Jx <= (x + r)) and (Jx >= (x - r))) and ((Jy <= (y + r)) and (Jy >= (y - r))):
+                        # if current circle centre is inside other circle then remove circle from list and add to
+                        # similar circle list
+                        if ((Jx <= (x + r)) and (Jx >= (x - r)) or (Jx == x)) and (
+                                (Jy <= (y + r)) and (Jy >= (y - r)) or (Jy == y)):
                             copy.remove(j)
                             SameCircles.append([Jx, Jy, Jr])
 
@@ -107,25 +75,67 @@ class XformPctCalibrate(XFormType):
                 if (xMean and yMean and rMean) != 0:
                     clustered.append([xMean, yMean, rMean])
 
-                copy.remove(c)
+                # copy.remove(c)
 
             print("cluster: ", clustered)
             return clustered
+
+    @staticmethod
+    def edge(img, kernel):
+        """Canny edge detector on image"""
+        # edge detector on the image -> need to do before circle detection
+        k = kernel
+        if img is None:
+            return None
+        elif type(img) is np.ndarray:
+            copy = img.copy()
+            # convert image 0-1 to 0-256 for opencv to work. 2^8 = 8bit image depth
+            copy = (copy * 255).astype(np.uint8)
+            blur = cv.GaussianBlur(copy, (k, k), 0)
+            edges = cv.Canny(image=blur, threshold1=100, threshold2=150)
+            return edges
+        else:
+            copy = img.copy()
+            for i in range(copy.channels):
+                c = copy.img[:, :, i]
+                # convert 0-1 numpy array image tp 0-256
+                c = (c * 255).astype(np.uint8)
+                # blur image
+                blur = cv.GaussianBlur(c, (k, k), 0)
+                # canny edge detection on blurred image
+                edges = cv.Canny(image=blur, threshold1=100, threshold2=150)
+                copy.img[:, :, i] = edges
+            return copy
+
+    @staticmethod
+    def drawCircles(img, Circles):
+        if Circles is None:
+            return None
+        else:
+            copy = img.copy()
+            rgb = copy.rgbImage()
+            for n in Circles:
+                x, y, r = n[0], n[1], n[2]
+                cv.circle(rgb.img, (x, y), r, (1, 0, 0), 1)
+                cv.circle(rgb.img, (x, y), 1, (0, 1, 0), 1)
+            return rgb
 
     def detect(self):
         pass
 
     def perform(self, node):
-        print(node.Kernel)
         data = node.getInput(0, Datum.IMG)
         if data is None:
             out = None
         else:
             img = data.copy()
-            k = node.Kernel
-            out = self.detect(img, k)
-            edges = self.edge(img, k)
-            out = out
+            if node.Detect:
+                print("pressed")
+                circles = self.detect(img, node)
+                clustered = self.cluster(circles)
+                out = self.drawCircles(img, clustered)
+            else:
+                out = img
         node.out = Datum(Datum.IMG, out)
         node.setOutput(0, node.out)
 
@@ -136,34 +146,40 @@ class HoughCircles(XformPctCalibrate):
 
     def __init__(self):
         super().__init__("Hough Circles", "calibration", "0.0.0")
-        # single input image
-        self.addInputConnector("", Datum.IMG)
-        # single output, it's an image for now but will need to decide on a
-        # output type at a later date -> the centre pixels of the targets
-        self.addOutputConnector("", Datum.IMG)
+
+    def init(self, node):
+        node.Kernel = 11
+        node.Detect = False
+        node.out = None
 
     def createTab(self, n, w):
+        # n -> node
+        # w -> window
         # standard tab for now, just outputs the image after the
         # xform is applied within the node
-        return PCTCalibTab(n, w)
+        return PCTHoughTab(n, w)
 
-    def detect(self, img, k):
+    def detect(self, img, node):
         if img is None:
             return None
         else:
-            copy = img.copy()
             circleAmount = []
-            rgb = copy.rgbImage()
-            # edge detector on image
-            edges = self.edge(copy, k)
             circleArray = []
+
+            k = node.Kernel
+            copy = img.copy()
+            edges = self.edge(copy, k)
+
             for i in range(edges.channels):
                 c = edges.img[:, :, i]
+
                 # convert edge image to a np array of uint
                 cArray = np.array(c).astype(np.uint8)
+
                 # hough circle detection
                 circles = cv.HoughCircles(cArray, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0,
                                           maxRadius=0)
+
                 if circles is not None:
                     circleNo = 0
                     circles = np.uint16(np.around(circles))
@@ -172,28 +188,172 @@ class HoughCircles(XformPctCalibrate):
                         circleNo += 1
                         x, y, r = j[0], j[1], j[2]
                         circleArray.append([x, y, r])
-                        # cv.circle(rgb.img, (x, y), r, (1, 0, 0), 1)
-                        # cv.circle(rgb.img, (x, y), 1, (0, 1, 0), 1)
+
                     circleAmount.append(circleNo)
-            ClusteredCircles = self.Cluster(circleArray)
-            for n in ClusteredCircles:
-                x, y, r = n[0], n[1], n[2]
-                cv.circle(rgb.img, (x, y), r, (1, 0, 0), 1)
-                cv.circle(rgb.img, (x, y), 1, (0, 1, 0), 1)
             print(circleAmount, np.mean(circleAmount))
-            # return rgb image with circles drawn on
-            return rgb
+            return circleArray
 
 
-class PCTCalibTab(pcot.ui.tabs.Tab):
+@xformtype
+class BlobDetector(XformPctCalibrate):
+
+    def __init__(self):
+        super().__init__("Blob detector", "calibration", "0.0.0")
+
+    def createTab(self, n, w):
+        # n -> node
+        # w -> window
+        # standard tab for now, just outputs the image after the
+        # xform is applied within the node
+        return PCTBlobDetector(n, w)
+
+    def init(self, node):
+        node.Detect = False
+        node.minThresh = 0
+        node.maxThresh = 255
+        node.areaFilter = False
+        node.minArea = 0
+        node.circularityFilter = False
+        node.minCircularity = 0.0
+        node.convexityFilter = False
+        node.minConvexity = 0.0
+        node.inertiaFilter = False
+        node.minInertia = 0.0
+
+        node.out = None
+
+    def detect(self, img, node):
+        if img is None:
+            return None
+        else:
+            print("detect by area: ", node.areaFilter)
+            copy = img.copy()
+
+            params = cv.SimpleBlobDetector_Params()
+
+            # thresholds
+            params.minThreshold = node.minThresh
+            params.maxThreshold = node.maxThresh
+            print("Max thresh: ", node.minThresh, "\n Min thresh: ", node.maxThresh)
+            # filter by area
+            params.filterByArea = node.areaFilter
+            params.minArea = node.minArea
+            print("\nFilter by area: ", node.areaFilter, "\n Min area: ", node.minArea)
+
+            # Filter by Circularity 
+            params.filterByCircularity = node.circularityFilter
+            params.minCircularity = node.minCircularity
+            print("\nFilter by circularity: ", node.circularityFilter, "\n Min circularity: ", node.minCircularity)
+
+            # Filter by Convexity 
+            params.filterByConvexity = node.convexityFilter
+            params.minConvexity = node.minConvexity
+            print("\nFilter by convexity: ", node.convexityFilter, "\n Min convexity: ", node.minConvexity)
+
+            # Filter by Inertia
+            params.filterByInertia = node.inertiaFilter
+            params.minInertiaRatio = node.minInertia
+            print("\nFilter by inertia: ", node.inertiaFilter, "\n Min inertia: ", node.minInertia)
+
+            # set up detector
+            detector = cv.SimpleBlobDetector_create(params)
+
+            # copy = self.thresh(copy)
+
+            coords = []
+            for i in range(copy.channels):
+                c = copy.img[:, :, i]
+                c = (c * 255).astype(np.uint8)
+                c = cv.GaussianBlur(c, (5, 5), 0)
+                ret, t = cv.threshold(c, 50, 255, cv.THRESH_BINARY_INV)
+                keypoints = detector.detect(t)
+                blobs = cv.drawKeypoints(c, keypoints, np.array([]), (255, 0, 0),
+                                         cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                plt.imshow(t)
+                plt.imshow(blobs)
+                plt.show()
+                for k in keypoints:
+                    x = k.pt[0]
+                    y = k.pt[1]
+                    # radius is diameter/2
+                    r = k.size / 2
+                    print(r)
+                    if r > 10:
+                        xyr = [np.uint16(np.around(x)), np.uint16(np.around(y)), np.uint16(np.around(r))]
+                        coords.append(xyr)
+
+            return coords
+
+
+class PCTBlobDetector(pcot.ui.tabs.Tab):
     def __init__(self, node, w):
-        super().__init__(w, node, "tabpctCalibration.ui")
-        self.w.KernelSize.editingFinished.connect(self.KernelChanged)
+        super().__init__(w, node, "tabpctBlobDetect.ui")
+        self.w.BlobDetect.clicked.connect(self.detect)
+        self.w.FilterArea.toggled.connect(self.filterByArea)
+        self.w.minAreaFilter.editingFinished.connect(self.areaSize)
+        self.w.FilterCircularity.toggled.connect(self.filterByCircularity)
+        self.w.minCircularityFilter.editingFinished.connect(self.minCircularity)
+        self.w.FilterConvexity.toggled.connect(self.filterByConvexity)
+        self.w.minConvexityFilter.editingFinished.connect(self.minConvexity)
+        self.w.FilterInertia.toggled.connect(self.filterByInertia)
+        self.w.minInertiaFilter.editingFinished.connect(self.minInertia)
+        self.w.MinThreshold.editingFinished.connect(self.minThresh)
+        self.w.MaxThreshold.editingFinished.connect(self.maxThresh)
         self.nodeChanged()
 
-    def KernelChanged(self):
+    def filterByArea(self):
         self.mark()
-        self.node.Kernel = self.w.KernelSize.value()
+        self.node.areaFilter = self.w.FilterArea.isChecked()
+        self.changed()
+
+    def areaSize(self):
+        self.mark()
+        self.node.minArea = self.w.minAreaFilter.value()
+        self.changed()
+
+    def filterByCircularity(self):
+        self.mark()
+        self.node.circularityFilter = self.w.FilterCircularity.isChecked()
+        self.changed()
+
+    def minCircularity(self):
+        self.mark()
+        self.node.minCircularity = self.w.minCircularityFilter.value()
+        self.changed()
+
+    def filterByConvexity(self):
+        self.mark()
+        self.node.circularityFilter = self.w.FilterConvexity.isChecked()
+        self.changed()
+
+    def minConvexity(self):
+        self.mark()
+        self.node.minConvexity = self.w.minConvexityFilter.value()
+        self.changed()
+
+    def filterByInertia(self):
+        self.mark()
+        self.node.inertiaFilter = self.w.FilterInertia.isChecked()
+        self.changed()
+
+    def minInertia(self):
+        self.mark()
+        self.node.minInertia = self.w.minInertiaFilter.value()
+        self.changed()
+
+    def minThresh(self):
+        self.mark()
+        self.node.minThresh = self.w.MinThreshold.value()
+        self.changed()
+
+    def maxThresh(self):
+        self.mark()
+        self.node.maxThresh = self.w.MaxThreshold.value()
+        self.changed()
+
+    def detect(self):
+        self.mark()
+        self.node.Detect = True
         self.changed()
 
     def onNodeChanged(self):
@@ -203,3 +363,26 @@ class PCTCalibTab(pcot.ui.tabs.Tab):
         self.w.canvas.display(self.node.out)
 
 
+class PCTHoughTab(pcot.ui.tabs.Tab):
+    def __init__(self, node, w):
+        super().__init__(w, node, "tabpctHoughTransform.ui")
+        # self.w.NAME.SIGNAL.connect(self.FUNCTION WHICH CHANGES VALUE)
+        self.w.KernelSize.editingFinished.connect(self.kernelChanged)
+        self.w.Detect.clicked.connect(self.detectState)
+        self.nodeChanged()
+
+    def kernelChanged(self):
+        self.mark()
+        self.node.Kernel = self.w.KernelSize.value()
+        self.changed()
+
+    def detectState(self):
+        self.mark()
+        self.node.Detect = True
+        self.changed()
+
+    def onNodeChanged(self):
+        self.w.canvas.setMapping(self.node.mapping)
+        self.w.canvas.setGraph(self.node.graph)
+        self.w.canvas.setPersister(self.node)
+        self.w.canvas.display(self.node.out)
